95d153e2ad38c6de6000ddf0076e8d022f4d0417
iperaltamrmilu <iperalta@mrmilu.com>
2022-05-18 15:00:55 +0200

    Create form to set nids and filters

--- src/Form/ReadableUrlForm.php
+++ src/Form/ReadableUrlForm.php
@@ -4,6 +4,7 @@ namespace Drupal\mrmilu_readable_url\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\Node;
 
 class ReadableUrlForm extends FormBase {
 
@@ -12,64 +13,118 @@ class ReadableUrlForm extends FormBase {
   }
 
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $langcode = \Drupal::languageManager()->getCurrentLanguage()->getId();
-    $langcodeString = \Drupal::languageManager()->getCurrentLanguage()->getName();
+   $form['#tree'] = TRUE;
+   $form['container'] = [
+     '#type' => 'details',
+     '#title' => $this->t('Url\'s'),
+     '#open' => TRUE,
+     '#prefix' => '<div id="urls-fieldset-wrapper">',
+     '#suffix' => '</div>',
+   ];
 
-    $form['container'] = array(
-      '#type' => 'container',
-      '#attributes' => array('class' => array('messages', 'messages--status')),
-    );
-    $form['container']['wrapper_language'] = array(
-      '#markup' => strtoupper($langcodeString) . ' Variables'
-    );
-    $form['block_content_inscription'] = [
-      '#type' => 'entity_autocomplete',
-      '#target_type' => 'node',
-      '#selection_handler' => 'default',
-      '#default_value' => null
+    $numUrls = $form_state->get('num_urls');
+    if ($numUrls === NULL) {
+      $stateUrls = \Drupal::state()->get('mrmilu_readable_url_num_urls');
+      $numUrls = $stateUrls != null ? $stateUrls : 1;
+      $form_state->set('num_urls', $numUrls);
+    }
+
+    $nodeValues = \Drupal::state()->get('mrmilu_readable_url_node');
+    $filterValues = \Drupal::state()->get('mrmilu_readable_url_filter');
+    for ($i = 0; $i < $numUrls; $i++) {
+      $form['container']['url'][$i] = [
+        '#type' => 'details',
+        '#title' => $this->t('Url') . ' ' . $i,
+        '#open' => TRUE,
+        '#prefix' => '<div id="filters-fieldset-wrapper-' . $i . '">',
+        '#suffix' => '</div>',
+      ];
+
+      $form['container']['url'][$i]['mrmilu_readable_url_node'] = [
+        '#title' => t('Node'),
+        '#type' => 'entity_autocomplete',
+        '#target_type' => 'node',
+        '#selection_handler' => 'default',
+        '#default_value' => isset($nodeValues[$i]) ? Node::load($nodeValues[$i]) : NULL
+      ];
+
+      $numFilters = $form_state->get(['num_filters', $i]);
+      if ($numFilters === NULL) {
+        $stateFilters = \Drupal::state()->get('mrmilu_readable_url_num_filters');
+        $numFilters = $stateFilters[$i] ?? 1;
+        $form_state->set(['num_filters', $i], $numFilters);
+      }
+
+      for ($j = 0; $j < $numFilters; $j++) {
+        $form['container']['url'][$i]['mrmilu_readable_url_filter'][$j] = [
+          '#type' => 'textarea',
+          '#title' => t('Filter') . ' ' . $j,
+          '#description' => t('Field key|Field value'),
+          '#default_value' => $filterValues[$i][$j] ?? NULL,
+        ];
+      }
+
+      // Filters actions
+      $form['container']['url'][$i]['actions'] = [
+        '#type' => 'actions',
+      ];
+      $form['container']['url'][$i]['actions']['add_filter'] = [
+        '#type' => 'submit',
+        '#value' => $this->t('Add filter'),
+        '#name' => 'add_filter_' . $i,
+        '#submit' => ['::addOneFilters'],
+        '#attributes' => [
+          'current_url' => $i,
+        ],
+        '#ajax' => [
+          'callback' => '::addmoreFiltersCallback',
+          'wrapper' => 'filters-fieldset-wrapper-' . $i,
+        ],
+      ];
+      if ($numFilters > 1) {
+        $form['container']['url'][$i]['actions']['remove_filter'] = [
+          '#type' => 'submit',
+          '#value' => $this->t('Remove filter'),
+          '#name' => 'remove_filter_' . $i,
+          '#submit' => ['::removeFiltersCallback'],
+          '#attributes' => [
+            'current_url' => $i,
+          ],
+          '#ajax' => [
+            'callback' => '::addmoreFiltersCallback',
+            'wrapper' => 'filters-fieldset-wrapper-' . $i,
+          ],
+        ];
+      }
+    }
+
+    // Url actions
+    $form['container']['actions'] = [
+      '#type' => 'actions',
     ];
-
-
-    $form['footer_hour'] = [
-      '#type' => 'textfield',
-      '#title' => t('Service hours'),
-      '#default_value' => \Drupal::state()->get('footer_hour'),
-      '#size' => 60,
-      '#maxlength' => 128,
-      '#required' => TRUE,
+    $form['container']['actions']['add_url'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Add url'),
+      '#submit' => ['::addOneUrls'],
+      '#ajax' => [
+        'callback' => '::addmoreUrlsCallback',
+        'wrapper' => 'urls-fieldset-wrapper',
+      ],
     ];
-    $form['footer_copyright'] = [
-      '#type' => 'textfield',
-      '#title' => t('Copyright'),
-      '#default_value' => \Drupal::state()->get('footer_copyright'),
-      '#size' => 60,
-      '#maxlength' => 128,
-      '#required' => TRUE,
-    ];
-    $form['footer_bottom_text'] = [
-      '#type' => 'textfield',
-      '#title' => t('Bottom text'),
-      '#default_value' => \Drupal::state()->get('footer_bottom_text'),
-      '#size' => 60,
-      '#maxlength' => 128,
-      '#required' => TRUE,
-    ];
+    if ($numUrls > 1) {
+      $form['container']['actions']['remove_url'] = [
+        '#type' => 'submit',
+        '#value' => $this->t('Remove url'),
+        '#submit' => ['::removeUrlsCallback'],
+        '#ajax' => [
+          'callback' => '::addmoreUrlsCallback',
+          'wrapper' => 'urls-fieldset-wrapper',
+        ],
+      ];
+    }
 
-    $fileId = \Drupal::state()->get('footer_bottom_icon');
-    $form['footer_bottom_icon'] = array(
-      '#type' => 'managed_file',
-      '#title' => t('Bottom icon'),
-      '#description' => t('Upload image, allowed extensions: jpg png'),
-      '#upload_location' => 'public://footer/',
-      '#upload_validators'  => array(
-        'file_validate_extensions' => array('jpg png'),
-      ),
-      '#default_value' => $fileId ? [$fileId] : NULL,
-      '#required' => TRUE,
-    );
-
-
-    $form['submit_button'] = [
+    // Filters actions
+    $form['actions']['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Submit'),
     ];
@@ -78,20 +133,86 @@ class ReadableUrlForm extends FormBase {
   }
 
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $vars = ['footer_hour', 'footer_copyright', 'footer_bottom_text'];
-    foreach ($vars as $var) \Drupal::state()->set($var, $form_state->getValue($var));
+    $values = $form_state->getValue(['container', 'url']);
+    $nodeValues = [];
+    $filterValues = [];
+    $filterIndex = [];
 
-    $fileID = $form_state->getValue('footer_bottom_icon');
-    $this->setFile('footer_bottom_icon', $fileID);
+    foreach ($values as $i => $value) {
+      $nodeValues[$i] = $value['mrmilu_readable_url_node'];
+      $filterValues[$i] = [];
+      foreach ($value['mrmilu_readable_url_filter'] as $j => $filter) {
+        $filterValues[$i][$j] = $filter;
+      }
+      $filterIndex[$i] = $j + 1;
+    }
 
-    \Drupal::messenger()->addMessage(t('Saved footer variables'));
+    \Drupal::state()->set('mrmilu_readable_url_node', $nodeValues);
+    \Drupal::state()->set('mrmilu_readable_url_filter', $filterValues);
+
+    // Auxiliar values to init form with indexes
+    $numUrls = $form_state->get('num_urls');
+    \Drupal::state()->set('mrmilu_readable_url_num_urls', $numUrls);
+    \Drupal::state()->set('mrmilu_readable_url_num_filters', $filterIndex);
+
+    \Drupal::messenger()->addMessage(t('Saved url\'s'));
   }
 
-  private function setFile($varState, $fileID) {
-    if ($fileID) {
-      $fileID = array_shift($fileID);
-      \Drupal::state()->set($varState, $fileID);
+  /**
+   * addMoreFunctions
+   */
+  public function addmoreUrlsCallback(array &$form, FormStateInterface $form_state) {
+    return $form['container'];
+  }
+
+  public function addmoreFiltersCallback(array &$form, FormStateInterface $form_state) {
+    $currentUrl = $this->getCurrentUrl($form_state);
+    return $form['container']['url'][$currentUrl];
+  }
+
+  /**
+   * addOne functions
+   */
+  public function addOneUrls(array &$form, FormStateInterface $form_state) {
+    $numUrls = $form_state->get('num_urls');
+    $addButton = $numUrls + 1;
+    $form_state->set('num_urls', $addButton);
+    $form_state->setRebuild();
+  }
+
+  public function addOneFilters(array &$form, FormStateInterface $form_state) {
+    $currentUrl = $this->getCurrentUrl($form_state);
+    $numFilters = $form_state->get(['num_filters', $currentUrl]);
+    $addButton = $numFilters + 1;
+    $form_state->set(['num_filters', $currentUrl], $addButton);
+    $form_state->setRebuild();
+  }
+
+  /**
+   * removeFunctions
+   */
+  public function removeUrlsCallback(array &$form, FormStateInterface $form_state) {
+    $numUrls = $form_state->get('num_urls');
+    if ($numUrls > 1) {
+      $removeButton = $numUrls - 1;
+      $form_state->set('num_urls', $removeButton);
     }
-    else \Drupal::state()->delete($varState);
+    $form_state->setRebuild();
+  }
+
+  public function removeFiltersCallback(array &$form, FormStateInterface $form_state) {
+    $currentUrl = $this->getCurrentUrl($form_state);
+    $numFilters = $form_state->get(['num_filters', $currentUrl]);
+    if ($numFilters > 1) {
+      $removeButton = $numFilters - 1;
+      $form_state->set(['num_filters', $currentUrl], $removeButton);
+    }
+    $form_state->setRebuild();
+  }
+
+  private function getCurrentUrl(FormStateInterface $form_state) {
+    $clickedElement = $form_state->getTriggeringElement();
+    $currentUrl = $clickedElement['#attributes']['current_url'];
+    return $currentUrl;
   }
 }
